25b80575c20975fff3a0b67f14089f2e171a47fc
Root <root@root.com>
2024-03-14 09:10:21 +0000

    approve mail page design

--- laravel_project/resources/views/backend/user/article/mail.blade.php
+++ laravel_project/resources/views/backend/user/article/mail.blade.php
@@ -1,29 +1,120 @@
 <!DOCTYPE html>
-<html>
-<head>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>$subject</title>
-    <style>   
-        .btn {
-            height: 30px;
-            color: #ffffff;
-            background-color: #009ef7;
-            text-align: center;
-            vertical-align: middle;
-            border-radius: 5%;
-            border:none;
-        }
-        a{
-            text-decoration: none;
-        }
+   
+    <style>
+      * {
+        margin: 0;
+        padding: 0;
+      }
+      .bg {
+        background: #edf2f7;
+      }
+      .invoice-box {
+        max-width: 730px;
+        margin: auto;
+        padding: 30px;
+        border: 1px solid #eee;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+        font-size: 16px;
+        line-height: 24px;
+        font-family: "Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif;
+        color: #555;
+        background: #fff;
+      }
+      .invoice-box tr td p{
+        padding: 7px 0px;
+      }
+      .invoice-box tr td .ans{
+        padding: 0px 25px;
+      }
     </style>
-</head>
-<body>    
-    
-    <p>Article Created By : {{$article_by}}.</p>
+  </head>
+  <body>
+    <div class="bg">
+      <table cellpadding="0" cellspacing="0" style="width: 100%;">
+        <tr>
+          <h1 style="text-align: center; padding: 25px 0px; width: 100%;">
+            Welcome to our community!
+          </h1>
+        </tr>
+      </table>
 
-    <p> Date : {{$date}}.</p>
+      <div class="invoice-box">
+        <table style="width:100%">
+          <tbody>
 
-    <button class="btn"><a href="{{ route('admin.items.edit', $item_id) }}" class="link" style="color:#ffffff;">Approve</a></button>
+            <tr>
+              <td style="font-size: 15px; line-height:30px; width:100%;">
+                <p style="padding: 10px 0px; font-weight: bold">
+                    Article Created By : {{$article_by}}.
+                </p>
+                <p style="padding: 10px 0px; font-weight: bold">
+                    Date : {{$date}}.
+                </p>
+              </td>
+            </tr>
 
-</body>
+            <tr>
+             <td style="text-align: center"><a href="{{ route('admin.items.edit', $item_id) }}">
+              <button type="button" style="
+              border-radius: 4px;
+              color: #fff;
+              display: inline-block;
+              overflow: hidden;
+              text-decoration: none;
+              background-color: #48bb78;
+              border-bottom: 8px solid #48bb78;
+              border-left: 18px solid #48bb78;
+              border-right: 18px solid #48bb78; 
+              border-top: 8px solid #48bb78;cursor:pointer;">Approve</button>
+            </a></td>
+            </tr>
+          </tbody>
+        </table>       
+        <table>
+          <tbody>
+            <tr>
+              <td style="font-size: 15px; line-height:30px; width:100%;">
+                <p style="padding-top: 10px;">Regards,</p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <table>
+          <tbody>
+            <tr>
+              <td style="font-size: 15px; line-height:30px; width:100%;">
+                <p style="padding: 0px 0px;">The CoachesHQ Family</p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <table cellpadding="0" cellspacing="0" style="margin: auto">
+        <tbody>
+          <tr>
+            <td style="padding: 32px">
+              <p
+                style="
+                  text-align: center;
+                  color: #b0adc5;
+                  font-size: 12px;
+                  text-align: center;
+                ">
+                © {{ $year }} Coach Directory. All rights reserved
+              </p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </body>
 </html>
+
